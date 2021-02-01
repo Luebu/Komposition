f01d0abdbdc4c7f4b7de386b2481657de0ed8a16
Akomi03 <55061661+Akomi03@users.noreply.github.com>
2021-02-01 20:08:30 +0100

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        Mannschaft m1 = new Mannschaft();
+        Mannschaft m2 = new Mannschaft();
+        Spiel sp = new Spiel(m1,m2);
     }
 }
